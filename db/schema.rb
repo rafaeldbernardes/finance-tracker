b75135e36f710f47f9b195356215879933999130
rafaeldelbel <rafaeldbernardes@gmail.com>
2016-09-29 22:46:11 -0300

    Update views and navigation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922004540) do
+ActiveRecord::Schema.define(version: 20160930011434) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
@@ -41,9 +44,11 @@ ActiveRecord::Schema.define(version: 20160922004540) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
